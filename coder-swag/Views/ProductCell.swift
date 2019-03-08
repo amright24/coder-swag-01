cc189cee09ddc2265d026fb55d07c9c532a36f92
Austin Rightnowar <austinrightnowar@Austins-MacBook-Pro.local>
2019-03-07 20:36:25 -0500

    added connections to product cell

---
+++ coder-swag/Views/ProductCell.swift
@@ -0,0 +1,15 @@
+//
+//  ProductCell.swift
+//  coder-swag
+//
+//  Created by Austin Rightnowar on 3/7/19.
+//  Copyright © 2019 Austin Rightnowar. All rights reserved.
+//
+
+import UIKit
+
+class ProductCell: UICollectionViewCell {
+    @IBOutlet weak var productImage: UIImageView!
+    @IBOutlet weak var productTitle: UILabel!
+    @IBOutlet weak var productPrice: UILabel!
+}
